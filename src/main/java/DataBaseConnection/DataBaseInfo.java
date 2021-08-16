cb4ccb95995a492c7a20d8fec2005d3a46553a84
Luka Samkharadze <lukesamkharadze@gmail.com>
2021-08-16 20:46:14 +0400

    Update DataBaseInfo.java

--- src/main/java/DataBaseConnection/DataBaseInfo.java
+++ src/main/java/DataBaseConnection/DataBaseInfo.java
@@ -3,8 +3,8 @@ package DataBaseConnection;
 
 public interface DataBaseInfo {
     String USER = "root"; // enter your username
-    String PASSWORD = "root123"; // enter your password
-    String BASE = "freeUniHubBase"; // enter your base
+    String PASSWORD = "root"; // enter your password
+    String BASE = "freeuni-hub"; // enter your base
     String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    String URL = "jdbc:mysql://localhost:3306/"; // needs base
+    String URL = "jdbc:mysql://freeuni-hub-db:3306/"; // needs base
 }
